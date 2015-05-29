47167b37bc593e1c494bee6b562d18e83f1b450b
Nicolas <belleme.nicolas@gmail.com>
2015-05-29 16:18:44 +0200

    changeDb

--- php/db/infoDb.php
+++ php/db/infoDb.php
@@ -1,5 +1,6 @@
 <?php
 	$user = "lesmelimelaws";
-	$pass = "ZOfybSPA";
-	$dsn = "mysql:host=lesmelimelaws.sql.free.fr;port=3306;dbname=lesmelimelaws";
+	$pass = "ZOfybSpA";
+        $host = "http://sql.free.fr/phpMyAdmin/";
+        $dsn = "mysql:host={$host};port=3306;dbname=lesmelimelaws";
 ?>
